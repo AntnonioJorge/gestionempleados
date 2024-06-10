6ac46a467a1ec38fa2e4e68565bbc2f6af6cefe3
ZeroxZ18 <ramonteodoromikonzo@gmail.com>
2024-06-10 16:00:58 +0100

    anadiendo la funcion para crear al empleado

--- dao/empleado.php
+++ dao/empleado.php
@@ -49,6 +49,15 @@
             return false;
         }
     }
+    function insertarEmpleados($conexion,$empleado){
+        try{
+            $sql="INSERT INTO empleado(nombreEmpleado,apellidosEmpleado,correoEmpleado,salarioEmpleado,rolEmpleado,edadEmpleado,dipEmpleado,fotoEmpleado)
+            VALUES('$empleado->nombreEmpleado','$empleado->apellidosEmpleado','$empleado->correoEmpleado','$empleado->salarioEmpleado','$empleado->rolEmpleado','$empleado->edadEmpleado','$empleado->dipEmpleado','$empleado->fotoEmpleado')";
+            $sql=$conexion->query($sql);
+        }catch(Exception $e){
+            echo "error al insertar empleado ".$e;
+        }
+    }
 
 
 
